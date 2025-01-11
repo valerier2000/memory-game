9db8e85944335257c9be56f16b709717dd40aa3b
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-11 17:17:34 +0100

    Refactor MemoryCard component for simpler emoji decoding and rendering

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,18 +1,10 @@
 import { decodeEntity } from "html-entities";
 
 export default function MemoryCard({ handleClick, data }) {
-  let emojiArray = ["🐶", "🐷", "🐙", "🐛", "🐵", "🐶", "🐷", "🐙", "🐛", "🐵"];
-
-  const decodedData = data
-    .map((d) => d.htmlCode[0])
-    .map((d) => decodeEntity(d));
-  console.log(decodedData);
-  emojiArray = [...decodedData];
-
-  const emojiEl = emojiArray.map((emoji, index) => (
+  const emojiEl = data.map((emoji, index) => (
     <li key={index} className="card-item">
       <button className="btn btn--emoji" onClick={handleClick}>
-        {emoji}
+        {decodeEntity(emoji.htmlCode[0])}
       </button>
     </li>
   ));
