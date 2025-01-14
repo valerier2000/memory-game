3c63ee8b3c3c077c8d50e0165a6396e7ee28434b
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-14 21:39:20 +0100

    Add focus to GameOver div using useRef, useEffect, .focus() and tabIndex for accessibility

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,15 @@
+import { useEffect, useRef } from "react";
 import RegularButton from "./RegularButton";
 
 export default function GameOver({ handleClick }) {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.focus();
+  }, []);
+
   return (
-    <div className="wrapper wrapper--accent">
+    <div className="wrapper wrapper--accent" ref={divRef} tabIndex={-1}>
       <p className="p--large">You've matched all the memory cards!</p>
       <RegularButton handleClick={handleClick}>Play again</RegularButton>
     </div>
