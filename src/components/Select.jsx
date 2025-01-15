fc775fef5d319b2593058335ae2907b3c5da3790
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-15 20:29:41 +0100

    Improve Select component by using destructuring in Object.entries

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,13 +1,13 @@
 import { data } from "../data/data";
 
 export default function Select({ handleChange }) {
-  Object.entries(data).map((data) => {
-    console.log(data[0]);
+  Object.entries(data).map(([key, value]) => {
+    console.log(key);
 
     const selectEl = (
-      <div className="form__inner-wrapper" key={data[0]}>
-        <label htmlFor={data[0]}>Select an emoji category</label>
-        <select name={data[0]} id={data[0]} onChange={handleChange}></select>
+      <div className="form__inner-wrapper" key={key}>
+        <label htmlFor={key}>Select an emoji category</label>
+        <select name={key} id={key} onChange={handleChange}></select>
       </div>
     );
 
